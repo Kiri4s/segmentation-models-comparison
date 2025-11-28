6897247b10d40780723f8245efc0660ea7e250ca
Semenovykh Kirill <semenovykh.ka@phystech.edu>
2025-11-28 21:08:47 +0300

    data resize for fpn and unet++ added

--- src/dataset.py
+++ src/dataset.py
@@ -16,6 +16,16 @@ class DeepGlobeDataset(Dataset):
     and corresponding masks.
     """
 
+    class_mapping = {
+        (  0, 255, 255): 0,  # Urban land
+        (255, 255,   0): 1,  # Agriculture land
+        (255,   0, 255): 2,  # Rangeland
+        (  0, 255,   0): 3,  # Forest land
+        (  0,   0, 255): 4,  # Water
+        (255, 255, 255): 5,  # Barren land
+        (  0,   0,   0): 6,  # Unknown
+    }
+
     def __init__(
         self,
         data_dir: str,
@@ -23,6 +33,7 @@ class DeepGlobeDataset(Dataset):
         val_size: float = 0.2,
         seed: int = 42,
         transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
     ):
         """
         Initializes the DeepGlobeDataset.
@@ -52,11 +63,11 @@ class DeepGlobeDataset(Dataset):
         else:
             raise ValueError("unsupported split")
 
-        self.transform = (
-            transforms.Compose([transforms.ToTensor()])
-            if transform is None
-            else transform
-        )
+        if transform is None:
+            self.transform = transforms.Compose([transforms.ToTensor()])
+        else:
+            self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self) -> int:
         """Returns the total number of samples in the dataset."""
@@ -84,43 +95,28 @@ class DeepGlobeDataset(Dataset):
         # Assuming the mask is an RGB image, we convert RGB values to class labels.
         # This mapping might need adjustment based on the actual dataset's color-to-class encoding.
         class_mask = np.zeros(mask_array.shape[:2], dtype=np.uint8)
-        class_mapping = {
-            (0, 255, 255): 0,  # Urban land
-            (255, 255, 0): 1,  # Agriculture land
-            (255, 0, 255): 2,  # Rangeland
-            (0, 255, 0): 3,  # Forest land
-            (0, 0, 255): 4,  # Water
-            (255, 255, 255): 5,  # Barren land
-            (0, 0, 0): 6,  # Unknown
-        }
 
-        for rgb, label in class_mapping.items():
+        for rgb, label in self.class_mapping.items():
             class_mask[(mask_array == rgb).all(axis=-1)] = label
+
         class_mask = torch.from_numpy(class_mask).long()
+        if self.target_transform:
+            class_mask = self.target_transform(class_mask.unsqueeze(0)).squeeze(0)
         return image, class_mask
 
-
-CLASS_MAPPING = {
-    (0, 255, 255): 0,  # Urban land
-    (255, 255, 0): 1,  # Agriculture land
-    (255, 0, 255): 2,  # Rangeland
-    (0, 255, 0): 3,  # Forest land
-    (0, 0, 255): 4,  # Water
-    (255, 255, 255): 5,  # Barren land
-    (0, 0, 0): 6,  # Unknown
-}
-
-
-def label_to_rgb_mask(label_mask: np.ndarray) -> Image.Image:
-    """
-    Converts a 2D label mask to a 3D RGB mask.
-    Args:
-        label_mask (np.ndarray): A 2D numpy array where each pixel is a class label.
-    Returns:
-        PIL.Image.Image: An RGB image representing the mask.
-    """
-    rgb_mask = np.zeros(label_mask.shape + (3,), dtype=np.uint8)
-    reverse_mapping = {label: rgb for rgb, label in CLASS_MAPPING.items()}
-    for label, rgb in reverse_mapping.items():
-        rgb_mask[label_mask == label] = rgb
-    return Image.fromarray(rgb_mask)
+    @staticmethod
+    def label_to_rgb_mask(label_mask: np.ndarray) -> Image.Image:
+        """
+        Converts a 2D label mask to a 3D RGB mask.
+        Args:
+            label_mask (np.ndarray): A 2D numpy array where each pixel is a class label.
+        Returns:
+            PIL.Image.Image: An RGB image representing the mask.
+        """
+        rgb_mask = np.zeros(label_mask.shape + (3,), dtype=np.uint8)
+        reverse_mapping = {
+            label: rgb for rgb, label in DeepGlobeDataset.class_mapping.items()
+        }
+        for label, rgb in reverse_mapping.items():
+            rgb_mask[label_mask == label] = rgb
+        return Image.fromarray(rgb_mask)
