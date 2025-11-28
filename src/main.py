2f30a4d35f99d066ae7c4731d83ed159b64ddb40
Semenovykh Kirill <semenovykh.ka@phystech.edu>
2025-11-28 14:36:28 +0300

    sanity check added && imports restructured

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 import torch
 import fire
 from dataset import DeepGlobeDataset
+from torch.utils.data import Subset
 from diceloss import DiceLoss
-from utils import train_and_validate
+from utils import train_and_validate, get_model
 from typing import TypedDict
-import segmentation_models_pytorch as smp
 from visualization import plot_loss, plot_iou
-from torchvision import transforms as T
-from tqdm import tqdm
 
 
 class StandartConfig(TypedDict):
@@ -16,88 +14,30 @@ class StandartConfig(TypedDict):
     val_size: float = 0.2
     transform = None
     batch_size: int = 1
-    learning_rate: float = 1e-5
+    learning_rate: float = 1e-4
     epochs: int = 1
-    encoder_name: str = "resnet34"
+    encoder_name: str = "resnet18"
     encoder_weights: str = "imagenet"
     activation: str = "logsoftmax"
     in_channels: int = 3
     classes: int = 7
     device: str = "mps"
     checkpoints_dir: str = "./checkpoints"
-
-
-def calculate_weights(data: torch.utils.data.DataLoader) -> torch.Tensor:
-    class_counts = torch.zeros(7, dtype=torch.long)
-    for _, mask in tqdm(data):
-        for i in range(7):
-            class_counts[i] += torch.sum(mask == i).item()
-
-    print(class_counts)
-    weights = 1.0 / (class_counts + 1e-3)
-
-    weights = weights / weights.mean()
-    print(weights)
-
-    return weights
+    freeze_encoder_layers: int = -2  # freeze encoder excluding 2 last layers
 
 
 def main(model_name: str = "unet", cfg=None):
     if cfg is None:
         cfg = StandartConfig
 
-    if model_name == "unet":
-        model = smp.Unet(
-            encoder_name=cfg.encoder_name,
-            encoder_weights=cfg.encoder_weights,
-            in_channels=cfg.in_channels,
-            classes=cfg.classes,
-            activation=cfg.activation,
-        )
-    elif model_name == "pspnet":
-        model = smp.PSPNet(
-            encoder_name=cfg.encoder_name,
-            encoder_weights=cfg.encoder_weights,
-            in_channels=cfg.in_channels,
-            classes=cfg.classes,
-            activation=cfg.activation,
-        )
-
-    elif model_name == "deeplab":
-        model = smp.DeepLabV3(
-            encoder_name=cfg.encoder_name,
-            encoder_weights=cfg.encoder_weights,
-            in_channels=cfg.in_channels,
-            classes=cfg.classes,
-            activation=cfg.activation,
-        )
-
-    elif model_name == "fpn":
-        cfg.transform = T.Compose([T.Resize((2464, 2464)), T.ToTensor()])
-        model = smp.FPN(
-            encoder_name=cfg.encoder_name,
-            encoder_weights=cfg.encoder_weights,
-            in_channels=cfg.in_channels,
-            classes=cfg.classes,
-            activation=cfg.activation,
-        )
-
-    elif model_name == "unet++":
-        cfg.transform = T.Compose([T.Resize((2464, 2464)), T.ToTensor()])
-        model = smp.UnetPlusPlus(
-            encoder_name=cfg.encoder_name,
-            encoder_weights=cfg.encoder_weights,
-            in_channels=cfg.in_channels,
-            classes=cfg.classes,
-            activation=cfg.activation,
-        )
-
-    else:
-        raise ValueError(
-            f"Model {model_name} is not supported. Choose from 'unet', 'pspnet', 'deeplab'."
-        )
-
+    model = get_model(model_name, cfg)
     model.to(cfg.device)
+    if cfg.freeze_encoder_layers:
+        print("freezing encoder layers")
+        for layer in list(model.encoder.children())[: cfg.freeze_encoder_layers]:
+            for param in layer.parameters():
+                param.requires_grad = False
+
     train_dataset = DeepGlobeDataset(
         data_dir=cfg.data_train,
         split="train",
@@ -114,6 +54,28 @@ def main(model_name: str = "unet", cfg=None):
         cfg.device
     )
 
+    # sanity check
+    train_and_validate(
+        model=model,
+        train_loader=torch.utils.data.DataLoader(
+            Subset(train_dataset, indices=(0, 1)),
+            batch_size=cfg.batch_size,
+            shuffle=True,
+        ),
+        val_loader=torch.utils.data.DataLoader(
+            Subset(val_dataset, indices=(0, 1)),
+            batch_size=cfg.batch_size,
+            shuffle=False,
+        ),
+        optimizer=torch.optim.Adam(model.parameters(), lr=cfg.learning_rate),
+        criterion=DiceLoss(),
+        epochs=cfg.epochs,
+        device=cfg.device,
+        model_name=model_name,
+        checkpoint_dir=cfg.checkpoints_dir,
+        sanity_check=True,
+    )
+
     model, history = train_and_validate(
         model=model,
         train_loader=torch.utils.data.DataLoader(
@@ -123,7 +85,7 @@ def main(model_name: str = "unet", cfg=None):
             val_dataset, batch_size=cfg.batch_size, shuffle=False
         ),
         optimizer=torch.optim.Adam(model.parameters(), lr=cfg.learning_rate),
-        criterion=torch.nn.CrossEntropyLoss(weight=weights),
+        criterion=DiceLoss(),
         epochs=cfg.epochs,
         device=cfg.device,
         model_name=model_name,
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,62 @@ import torch
 import json
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from torchmetrics.segmentation import MeanIoU
+import segmentation_models_pytorch as smp
+from torchvision import transforms as T
+
+
+def get_model(model_name: str, cfg):
+    if model_name == "unet":
+        model = smp.Unet(
+            encoder_name=cfg.encoder_name,
+            encoder_weights=cfg.encoder_weights,
+            in_channels=cfg.in_channels,
+            classes=cfg.classes,
+            activation=cfg.activation,
+        )
+    elif model_name == "pspnet":
+        model = smp.PSPNet(
+            encoder_name=cfg.encoder_name,
+            encoder_weights=cfg.encoder_weights,
+            in_channels=cfg.in_channels,
+            classes=cfg.classes,
+            activation=cfg.activation,
+        )
+
+    elif model_name == "deeplab":
+        model = smp.DeepLabV3(
+            encoder_name=cfg.encoder_name,
+            encoder_weights=cfg.encoder_weights,
+            in_channels=cfg.in_channels,
+            classes=cfg.classes,
+            activation=cfg.activation,
+        )
+
+    elif model_name == "fpn":
+        cfg.transform = T.Compose([T.Resize((2464, 2464)), T.ToTensor()])
+        model = smp.FPN(
+            encoder_name=cfg.encoder_name,
+            encoder_weights=cfg.encoder_weights,
+            in_channels=cfg.in_channels,
+            classes=cfg.classes,
+            activation=cfg.activation,
+        )
+
+    elif model_name == "unet++":
+        cfg.transform = T.Compose([T.Resize((2464, 2464)), T.ToTensor()])
+        model = smp.UnetPlusPlus(
+            encoder_name=cfg.encoder_name,
+            encoder_weights=cfg.encoder_weights,
+            in_channels=cfg.in_channels,
+            classes=cfg.classes,
+            activation=cfg.activation,
+        )
+
+    else:
+        raise ValueError(
+            f"Model {model_name} is not supported. Choose from 'unet', 'pspnet', 'deeplab'."
+        )
+    return model
 
 
 def train_and_validate(
@@ -17,14 +72,22 @@ def train_and_validate(
     device: str,
     model_name: str,
     checkpoint_dir: str = "./checkpoints",
+    sanity_check=False,
 ) -> Tuple[torch.nn.Module, Dict[str, list]]:
-    history = {"train_loss": [], "val_loss": [], "val_meanIoU": [], "tl": [], "vl": []}
+    history = {
+        "train_loss": [],
+        "val_loss": [],
+        "val_meanIoU": [],
+        "tl": [],
+        "vl": [],
+        "viou": [],
+    }
     best_val_loss = float("inf")
-    IoU = MeanIoU(num_classes=7, input_format="index").to(device)
     for epoch in range(epochs):
         model.train()
         running_loss = 0.0
         train_bar = tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs} [Train]")
+        n_step = 0
         for images, masks in train_bar:
             images, masks = images.to(device), masks.to(device)
 
@@ -34,15 +97,18 @@ def train_and_validate(
             loss.backward()
             optimizer.step()
 
+            n_step += 1
             running_loss += loss.item()
-            train_bar.set_postfix(loss=running_loss / len(train_loader))
-
-            history["train_loss"].append(running_loss / len(train_loader))
+            train_bar.set_postfix(loss=running_loss / n_step)
             history["tl"].append(loss.item())
+
+        history["train_loss"].append(running_loss / len(train_loader))
 
         model.eval()
         val_loss = 0.0
+        val_iou = 0.0
         val_bar = tqdm(val_loader, desc=f"Epoch {epoch + 1}/{epochs} [Val]")
+        n_step = 0
         with torch.no_grad():
             for images, masks in val_bar:
                 images, masks = images.to(device), masks.to(device)
@@ -51,22 +117,25 @@ def train_and_validate(
                 val_loss += loss.item()
 
                 predicted = (torch.argmax(outputs, dim=1)).long()
+                n_step += 1
+                val_bar.set_postfix(val_loss=val_loss / n_step)
 
-                # val_bar.set_postfix(
-                #    val_loss=val_loss / len(val_loader)
-                # )
-
-                val_loss /= len(val_loader)
-                iou = IoU(predicted, masks)
+                iou = compute_IoU(predicted, masks)
+                val_iou += iou.item()
                 history["vl"].append(loss.item())
-                history["val_meanIoU"].append(iou.item())
+                history["viou"].append(iou.item())
 
-        history["val_loss"].append(val_loss)
+        history["val_meanIoU"].append(val_iou / len(val_loader))
+        history["val_loss"].append(val_loss / len(val_loader))
 
         print(
             f"Epoch {epoch + 1}/{epochs}, Train Loss: {history['train_loss'][-1]:.4f}, "
             f"Val Loss: {val_loss:.4f}, Val Mean IoU: {iou.item():.4f}"
         )
+
+        if sanity_check:
+            print("Sanity Check completed")
+            return
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
@@ -84,3 +153,28 @@ def save_model(model: torch.nn.Module, path: str) -> None:
 def save_history(history: Dict[str, list], path: str) -> None:
     with open(path, "w") as f:
         json.dump(history, f)
+
+
+def calculate_weights(data: torch.utils.data.DataLoader) -> torch.Tensor:
+    class_counts = torch.zeros(7, dtype=torch.long)
+    for _, mask in tqdm(data):
+        for i in range(7):
+            class_counts[i] += torch.sum(mask == i).item()
+
+    print(class_counts)
+    weights = 1.0 / (class_counts + 1e-3)
+
+    weights = weights / weights.mean()
+    print(weights)
+
+    return weights
+
+
+def compute_IoU(
+    pred_mask: torch.tensor, mask: torch.tensor, num_classes: int = 7
+) -> torch.tensor:
+    tp, fp, fn, tn = smp.metrics.get_stats(
+        pred_mask, mask, mode="multiclass", num_classes=num_classes
+    )
+    image_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+    return image_iou
